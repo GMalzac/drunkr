3888a6db6bb158719bdbcb9f366272e0051f72dc
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-09-18 17:36:08 +0100

    Add validations to Drink

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,7 +1,13 @@
 class Drink < ApplicationRecord
-  has_many :user_drinks
-  has_many :users, through: :user_drinks
+  # has_many :user_drinks
+  # has_many :users, through: :user_drinks
 
-  has_many :event_drinks
-  has_many :events, through: :event_drinks
+  # has_many :event_drinks
+  # has_many :events, through: :event_drinks
+
+  validates :name, presence: true
+  validates :volume, presence: true
+  validates :alcohol_rate, presence: true
+  # validates :alcohol, presence: true
+  # validates :photo, presence: true
 end
