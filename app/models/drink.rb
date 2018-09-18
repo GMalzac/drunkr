ac13dec0d511d19277a7dcdb0069439c805896a0
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-09-18 17:19:25 +0100

    Fix all models validation typo

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,7 +1,7 @@
 class Drink < ApplicationRecord
   has_many :user_drinks
-  has_many :users, through :user_drinks
+  has_many :users, through: :user_drinks
 
   has_many :event_drinks
-  has_many :events, through :event_drinks
+  has_many :events, through: :event_drinks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :user_drinks, dependent: :destroy
-  has_many :drinks, through :user_drinks
+  has_many :drinks, through: :user_drinks
 
   has_many :user_events
-  has_many :events, through :user_events
+  has_many :events, through: :user_events
 
   validates :email, presence: true, uniqueness: true
 end
